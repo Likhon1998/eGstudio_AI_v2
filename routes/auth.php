022c614ca6d749eeab7b5ea6481d215d88a3e448
Md. Nahid Hosain Likhon <n.h.likhon98@gmail.com>
2026-04-30 13:48:21 +0600

    fix: remove breeze password routes to fix collision with custom OTP flow

--- routes/auth.php
+++ routes/auth.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
@@ -22,17 +20,10 @@ Route::middleware('guest')->group(function () {
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-        ->name('password.request');
-
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-        ->name('password.email');
-
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-        ->name('password.reset');
-
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-        ->name('password.store');
+    // ----------------------------------------------------------------------
+    // DEFAULT BREEZE PASSWORD ROUTES REMOVED TO PREVENT COLLISION.
+    // The new 3-step OTP Glassmorphism routes are now safely in web.php
+    // ----------------------------------------------------------------------
 });
 
 Route::middleware('auth')->group(function () {
